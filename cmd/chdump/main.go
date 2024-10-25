1f2738fade150bf2d1c7c3172da9b6c69a022e1e
kovel <kovelav@gmail.com>
2024-10-26 00:22:41 +0300

    secure login

--- cmd/chdump/main.go
+++ cmd/chdump/main.go
@@ -1,40 +1,81 @@
 package main
 
 import (
-	"database/sql"
+	"context"
+	"crypto/tls"
 	"fmt"
-	"log"
-	"os"
-
+	"github.com/ClickHouse/clickhouse-go/v2"
 	_ "github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"log"
+	"net/url"
+	"os"
 )
 
-func main() {
-	// Replace these with your actual database credentials and connection details
-	const (
-		address  = "tcp://localhost:9000"
-		username = "default"
-		password = ""
-		database = "default"
+func connect(ctx context.Context, host, user, password, db string) (driver.Conn, error) {
+	var (
+		conn, err = clickhouse.Open(&clickhouse.Options{
+			Addr: []string{host},
+			Auth: clickhouse.Auth{
+				Database: db,
+				Username: user,
+				Password: password,
+			},
+			ClientInfo: clickhouse.ClientInfo{
+				Products: []struct {
+					Name    string
+					Version string
+				}{
+					{Name: "an-example-go-client", Version: "0.1"},
+				},
+			},
+
+			Debugf: func(format string, v ...interface{}) {
+				fmt.Printf(format, v)
+			},
+			TLS: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		})
 	)
 
-	clickhouseDSN := os.Args[1]
+	if err != nil {
+		return nil, err
+	}
 
+	if err := conn.Ping(ctx); err != nil {
+		if exception, ok := err.(*clickhouse.Exception); ok {
+			fmt.Printf("Exception [%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+		}
+		return nil, err
+	}
+	return conn, nil
+}
+
+func main() {
+	ctx := context.Background()
 	// Establish a connection to the database
-	// connStr := fmt.Sprintf("%s/%s?username=%s&password=%s", address, database, username, password)
-	db, err := sql.Open("clickhouse", clickhouseDSN)
+
+	u, err := url.Parse(os.Args[1])
+	if err != nil {
+		log.Fatalf("cannot parse url: %v", err)
+	}
+
+	password, _ := u.User.Password()
+
+	db, err := connect(ctx, u.Host, u.User.Username(), password, u.Path[1:])
 	if err != nil {
 		log.Fatalf("Failed to open database: %v", err)
 	}
 	// Ping the database to ensure connection is established
-	if err := db.Ping(); err != nil {
+	if err := db.Ping(ctx); err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
 	defer db.Close()
 
 	// Query to retrieve table names in the specified database
 	tableNamesQuery := "SHOW TABLES"
-	rows, err := db.Query(tableNamesQuery)
+	rows, err := db.Query(ctx, tableNamesQuery)
 	if err != nil {
 		log.Fatalf("Failed to execute query: %v", err)
 	}
@@ -47,7 +88,7 @@ func main() {
 		}
 		// Query to retrieve the DDL of each table
 		showCreateTableQuery := fmt.Sprintf("SHOW CREATE TABLE `%s`", tableName)
-		tableDDL, err := db.Query(showCreateTableQuery)
+		tableDDL, err := db.Query(ctx, showCreateTableQuery)
 		if err != nil {
 			log.Fatalf("Failed to get DDL for table %s: %v", tableName, err)
 		}
